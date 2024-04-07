8102266c22643756f493c5c54b8cdab26c7aa1fa
Alexander Mulyar <mulyar.alexander0503@yandex.ru>
2024-04-07 19:15:17 +0500

    First commit

---
+++ conf.php
@@ -0,0 +1,6 @@
+<?php
+    $CONF["company"] = "AMSFiles";
+    $CONF["copyright"] = "© 2024 Alexander Mulyar";
+
+    $CONF["pathFiles"] = "/var/www/web_pr/filesUpload";
+?>
---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+    include "conf.php";
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> <?php print($CONF["company"]); ?> </title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" href="img/favicon.png">
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+
+    <body>
+        <header>
+            <div class="header_logo">
+                <a href="./"> <img src="img/favicon.png" width="75px" height="75px"> </a>
+            </div>
+            <div class="header_link">
+                <a href="./"> <b> <i> <?php print($CONF["company"]); ?> </i> </b> </a>
+            </div>
+        </header>
+
+        <footer>
+            <p>
+                <?php print($CONF["copyright"]); ?>
+            </p>
+        </footer>
+    </body>
+</html>
