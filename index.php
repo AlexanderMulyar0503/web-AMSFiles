d5f8a5e45abfed72a1c8302aa8b9fbcab7495339
Alexander Mulyar <mulyar.alexander0503@yandex.ru>
2024-04-09 13:52:47 +0500

    Creating a panel

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="icon" href="img/favicon.png">
         <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/index.css">
     </head>
 
     <body>
@@ -21,6 +22,18 @@
                 <a href="./"> <b> <i> <?php print($CONF["company"]); ?> </i> </b> </a>
             </div>
         </header>
+
+        <div class="filesPanel">
+            <div class="createDir">
+                <a href="#"> <img src="img/type_folder.png" width="50px" height="50px"> </a>
+                <a href="#">Создать папку</a>
+            </div>
+
+            <div class="uploadFile">
+                <a href="#"> <img src="img/type_file.png" width="50px" height="50px"> </a>
+                <a href="#">Загрузить файл</a>
+            </div>
+        </div>
 
         <footer>
             <p>
